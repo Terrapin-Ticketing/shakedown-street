fb8b79391ebe24e7b2bb281988f818c6b8d32c81
Michael Reeder <reeder@knoesis.org>
2017-09-30 17:59:21 -0400

    added allow origin to config

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json({limit: '3mb'}));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', config.allowOrigin);
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
   res.header('Access-Control-Allow-Headers', 'Authorization, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
