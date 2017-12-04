d23962ddae68800db520bde140d0f9eb1a6ae9e6
Michael Reeder <reeder@knoesis.org>
2017-12-04 16:21:31 -0500

    saving etherum

--- controllers/event.js
+++ controllers/event.js
@@ -5,10 +5,7 @@ const EventModel = require('../models/event');
 class EventApi {
   getEventInfo(eventAddress) {
     return new Promise((resolve, reject) => {
-      console.log('in here', eventAddress);
       EventModel.findOne({eventAddress: eventAddress}).exec((err, event) => {
-        console.log('herer');
-        console.log(err, event);
         if (!event) return reject(new Error('This event doesn\'t exist.'));
         return resolve(event);
       });
