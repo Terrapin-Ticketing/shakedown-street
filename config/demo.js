f12d32bf3c8cb882e77473bfa9fab9ba12d034f0
Michael Reeder <reeder@knoesis.org>
2017-12-21 16:20:18 -0500

    changed access

--- config/demo.js
+++ config/demo.js
@@ -20,22 +20,3 @@ module.exports = {
   infopass,
   'clientDomain': 'https://terrapintickets.io'
 };
-
-
-
-
-{
-    "date" : "3/4/2018",
-    "name" : "Columbus Brew Grass Festival",
-    "urlSafe" : "BrewGrassFestival",
-    "description" : "Greensky Bluegrass, Billy Strings, Larry Keel, Rumpke Mountain Boys, Kitchen Dwellers, Blue Moon Soup, Still Shine",
-    "imageUrl" : "http://liveatthebluestone.com/wp-content/uploads/2017/12/24068068_528690924147257_2284411860158418511_n.png",
-    "createrId" : ObjectId("5a3ab43dbc35b139aabbaff6"),
-    "venue" : {
-        "name" : "The Bluestone",
-        "address" : "583 E Broad St",
-        "city" : "Columbus",
-        "state" : "OH",
-        "zip" : "43215"
-    }
-}
