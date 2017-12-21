1e753b7ddf1836ac4c7878709add7d3de43efd02
Michael Reeder <reeder@knoesis.org>
2017-12-21 16:18:00 -0500

    removed trust proxy

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.set('trust proxy', 1); // trust first proxy
+// app.set('trust proxy', 1); // trust first proxy
 
 mongoose.connect('mongodb://localhost/terrapin', { useMongoClient: true, promiseLibrary: bluebird });
 
