9f44979e54c35fc0669b5ceadaee994da4c697b4
Kevin Mulcrone <kaybesee@gmail.com>
2018-04-23 21:15:14 -0400

    added populate script

--- bin/populate-db.js
+++ bin/populate-db.js
@@ -1,6 +1,11 @@
-import Event from 'wherever event is'
+require('../src/_utils/bootstrap')
+import Event from '../src/events/controller'
+import cinciRegisterTestEvent from '../src/integrations/cinci-register/test-event'
 
 
 (async function() {
-  await Event.createEvent()
+  console.log('start create event')
+  await Event.createEvent(cinciRegisterTestEvent)
+  console.log('end create event')
+  process.exit()
 })()
