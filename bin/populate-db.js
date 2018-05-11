6c0c9b1aa21f01e6228a9ca922936bddebf590e9
Michael Reeder <reeder@knoesis.org>
2018-05-11 02:01:00 -0400

    new pop

--- bin/populate-db.js
+++ bin/populate-db.js
@@ -1,11 +1,40 @@
-require('../src/_utils/bootstrap')
+const { mongoose } = require('../src/_utils/bootstrap')
 import Event from '../src/events/controller'
+import User from '../src/users/controller'
 import cinciRegisterTestEvent from '../src/integrations/cinci-register/test-event'
+import CinciRegister from '../src/integrations/cinci-register/integration'
 
+import missionTixTestEvent from '../src/integrations/mission-tix/test-event'
+import MissinTix from '../src/integrations/cinci-register/integration'
 
 (async function() {
-  console.log('start create event')
-  await Event.createEvent(cinciRegisterTestEvent)
-  console.log('end create event')
+  await clearDb()
+
+  const barcode = await createCinciReigsterTicket()
+  console.log('Barcode: ', barcode)
   process.exit()
 })()
+
+async function createMissionTixTicket() {
+  const user = await User.createUser('test1@test.com', 'test')
+  const event = await Event.createEvent(missionTixTestEvent)
+
+
+}
+
+async function createCinciReigsterTicket() {
+  const user = await User.createUser('test@test.com', 'test')
+  const event = await Event.createEvent(cinciRegisterTestEvent)
+  const ticketType = Object.keys(event.ticketTypes)[0]
+  const cinciRegisterBarcode = await CinciRegister.issueTicket(event, user, ticketType)
+  // drop database
+  await mongoose.dropCollection('users')
+  await mongoose.dropCollection('tickets')
+  return cinciRegisterBarcode
+}
+
+async function clearDb() {
+  await mongoose.dropCollection('events')
+  await mongoose.dropCollection('users')
+  await mongoose.dropCollection('tickets')
+}
