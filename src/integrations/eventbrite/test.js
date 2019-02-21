22a57d5ea8165b947868f691718c9ce148d56eea
Michael Reeder <reeder@knoesis.org>
2019-02-20 20:50:46 -0800

    init eventbrite

---
+++ src/integrations/eventbrite/test.js
@@ -0,0 +1,68 @@
+const { mongoose } = require('../../_utils/bootstrap')
+
+import Event from '../../events/controller'
+import User from '../../users/controller'
+
+import EventBriteIntegration from './integration'
+import testEventConfig from './test-event'
+
+describe('Cinci Ticket Intergration', () => {
+  beforeAll(async() => {
+    await mongoose.dropCollection('events')
+    await mongoose.dropCollection('users')
+    await mongoose.dropCollection('tickets')
+  })
+  afterEach(async() => {
+    await mongoose.dropCollection('events')
+    await mongoose.dropCollection('users')
+    await mongoose.dropCollection('tickets')
+  })
+
+  it.only('should login', async() => {
+    const event = await Event.createEvent(testEventConfig)
+
+    const apiKey = 'DJNFCLSHCLWR22XSYZ73'
+
+    const rawCookies = await EventBriteIntegration.login(apiKey, '', event)
+
+
+    // expect(rawCookies.includes('UserSession')).toBeTruthy()
+  }, 10000)
+
+  // it('should return true for valid barcode', async() => {
+  //   const barcode = '0222226482260290522229'
+  //   const event = await Event.createEvent(cinciTicketTestEvent)
+  //   const isValidTicket = await CinciTicketIntegration.isValidTicket(barcode, event)
+  //   expect(isValidTicket).toBeTruthy()
+  // }, 10000)
+  //
+  // it('should return false for invalid barcode', async() => {
+  //   const barcode = 'not-a-barcode'
+  //   const event = await Event.createEvent(cinciTicketTestEvent)
+  //   const isValidTicket = await CinciTicketIntegration.isValidTicket(barcode, event)
+  //   expect(isValidTicket).toBeFalsy()
+  // }, 10000)
+  //
+  // it('should issue ticket', async() => {
+  //   const event = await Event.createEvent(cinciTicketTestEvent)
+  //   const user = await User.createUser('test@test.com', 'test')
+  //   const barcode = await CinciTicketIntegration.issueTicket(event, user, 'REG')
+  //   const isValidTicket = await CinciTicketIntegration.isValidTicket(barcode, event)
+  //   expect(isValidTicket).toBeTruthy()
+  // }, 100000)
+  //
+  // it('should lookup ticket type and price from barcode', async() => {
+  //   const barcode = '0000001860012019400002'
+  //   const event = await Event.createEvent(cinciTicketTestEvent)
+  //   const ticketInfo = await CinciTicketIntegration.getTicketInfo(barcode, event)
+  //   expect(ticketInfo).toBeTruthy()
+  // }, 100000)
+  //
+  // it('should deactivate a barcode', async() => {
+  //   const event = await Event.createEvent(cinciTicketTestEvent)
+  //   const user = await User.createUser('test@test.com', 'test')
+  //   const barcode = await CinciTicketIntegration.issueTicket(event, user, 'REG')
+  //   const deactivatedSuccess = await CinciTicketIntegration.deactivateTicket(event._id, barcode)
+  //   expect(deactivatedSuccess).toBeTruthy()
+  // }, 100000)
+})
